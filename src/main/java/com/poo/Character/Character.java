8a91e29ba8ef9e8fc879163b9f8d797b994918ba
Edwin Quintero <sawiedwin@hotmail.com>
2024-02-20 17:54:32 -0500

    Creacion Caracteres

--- src/main/java/com/poo/Character/Character.java
+++ src/main/java/com/poo/Character/Character.java
@@ -7,7 +7,7 @@ class heroe {
     int life;
     int shield;
 
-    
+    // caracter
     public heroe(String name, int level, int force, int life, int shield) {
         this.name = name;
         this.level = level;
