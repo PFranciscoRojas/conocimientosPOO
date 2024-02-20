1a2df05024c71ec83c31cbae6705a579e3d5c3f4
Edisonnarvaez <124762853+Edisonnarvaez@users.noreply.github.com>
2024-02-20 10:08:57 -0500

    Update Store.java

--- src/main/java/com/poo/Store.java
+++ src/main/java/com/poo/Store.java
@@ -2,7 +2,7 @@ package com.poo;
 
 public class Store {
     public void visitStore() {
-        // Implementa la lógica para visitar la tienda
+        
         System.out.println("Visiting the store!");
     }
 }
