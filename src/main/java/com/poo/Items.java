5431fa6a175ae91542d34fef1373f1f7a610fc9b
Laboratorio Informática 4 - Pasto <labinf4.pasto@CAMPUS.local>
2024-02-20 18:10:35 -0500

    Cambios

---
+++ src/main/java/com/poo/Items.java
@@ -0,0 +1,64 @@
+public class Items {
+    private String nombre;
+    private int peso;
+    private int valor;
+    private String descripcion;
+    private boolean esConsumible;
+
+    // Constructor
+    public Items(String nombre, int peso, int valor, String descripcion, boolean esConsumible) {
+        this.nombre = nombre;
+        this.peso = peso;
+        this.valor = valor;
+        this.descripcion = descripcion;
+        this.esConsumible = esConsumible;
+    }
+
+    // Métodos de acceso (getters y setters)
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getPeso() {
+        return peso;
+    }
+
+    public void setPeso(int peso) {
+        this.peso = peso;
+    }
+
+    public int getValor() {
+        return valor;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public boolean esConsumible() {
+        return esConsumible;
+    }
+
+    public void setEsConsumible(boolean esConsumible) {
+        this.esConsumible = esConsumible;
+    }
+
+
+    @Override
+    public String toString() {
+        return "Item [nombre=" + nombre + ", peso=" + peso + ", valor=" + valor +
+               ", descripcion=" + descripcion + ", esConsumible=" + esConsumible + "]";
+    }
+}
