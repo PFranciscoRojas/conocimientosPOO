44f9dc98a525d29ca20e4385b3efd804e82d4adf
Edisonnarvaez <124762853+Edisonnarvaez@users.noreply.github.com>
2024-02-20 10:08:28 -0500

    Update Inventory.java

--- src/main/java/com/poo/Inventory.java
+++ src/main/java/com/poo/Inventory.java
@@ -2,7 +2,7 @@ package com.poo;
 
 public class Inventory {
     public void showInventory() {
-        // Implementa la lógica para mostrar el inventario
+        
         System.out.println("Displaying the inventory!");
     }
 }
