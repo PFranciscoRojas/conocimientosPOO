4ca88bc37746f52a3654f5766e4c68675132c5be
Edisonnarvaez <124762853+Edisonnarvaez@users.noreply.github.com>
2024-02-20 10:08:11 -0500

    Update Fight.java

--- src/main/java/com/poo/Fight.java
+++ src/main/java/com/poo/Fight.java
@@ -2,7 +2,7 @@ package com.poo;
 
 public class Fight {
     public void startFight() {
-        // Implementa la lógica para iniciar un combate
+        
         System.out.println("Starting the fight!");
     }
 }
