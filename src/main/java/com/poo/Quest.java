88f05e76813eaec589ff477b61a94d5096cd7e6a
Laboratorio Informática 4 - Pasto <labinf4.pasto@CAMPUS.local>
2024-02-21 18:16:23 -0500

    v2 Quest

--- src/main/java/com/poo/Quest.java
+++ src/main/java/com/poo/Quest.java
@@ -15,6 +15,37 @@ public class Quest {
         this.reward = reward;
         this.status = false;
     }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    public String getActor() {
+        return actor;
+    }
+    public void setActor(String actor) {
+        this.actor = actor;
+    }
+    public String getReward() {
+        return reward;
+    }
+    public void setReward(String reward) {
+        this.reward = reward;
+    }
+    public boolean isStatus() {
+        return status;
+    }
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+    
     public void detailsQuest(){
         System.out.println("Quest: "+name);
         System.out.println("Description: "+description);
@@ -23,6 +54,12 @@ public class Quest {
     public void viewStatus(){
         this.status = true;
         System.out.println("Quest Completed!");
+    }
+    public void viewActor(){
+        System.out.println("Actor: "+actor);
+    }
+    public void viewReward(){
+        System.out.println("Reward: "+reward);
     }
 }
 
